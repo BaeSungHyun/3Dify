6c7ae165dce4edd8108220571f4a226f2d003e89
BaeSungHyun <shijr0725@gmail.com>
2024-03-03 15:07:43 +0900

    Action Bar added

--- app/src/main/java/bae/part2/a3dify/app_entry/MainActivity.kt
+++ app/src/main/java/bae/part2/a3dify/app_entry/MainActivity.kt
@@ -3,10 +3,12 @@ package bae.part2.a3dify.app_entry
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import bae.part2.a3dify.R
 import bae.part2.a3dify.databinding.ActivityMainBinding
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -16,11 +18,12 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // ######### Doesn't need this because it's inside FrameLayout now ##########
         // Adding toolbar support
-        binding.mainToolbar.title = "3Dify"
-        setSupportActionBar(binding.mainToolbar)
+        // binding.mainToolbar.title = "3Dify"
+        // val toolbar = findViewById<MaterialToolbar>(R.id.topAppBar)
+        // setSupportActionBar(toolbar)
         // How to set supportActionBar
-        supportActionBar?.setDisplayHomeAsUpEnabled(false)
 
         if (savedInstanceState == null) { // to ensure that the fragment is added only once
             supportFragmentManager.commit {
