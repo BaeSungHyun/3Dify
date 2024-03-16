3bd56aa7d1f4432f84b633d6f9e26b4921f6e5c8
BaeSungHyun <shijr0725@gmail.com>
2024-03-16 22:17:48 +0900

    Finished signin for Google and Google Drive. No backend

--- app/src/main/java/com/part2/a3dify/app_entry/LoginFragment.kt
+++ app/src/main/java/com/part2/a3dify/app_entry/LoginFragment.kt
@@ -1,6 +1,7 @@
 package com.part2.a3dify.app_entry
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -8,19 +9,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
-import androidx.credentials.CredentialManager
-import androidx.credentials.CustomCredential
-import androidx.credentials.GetCredentialRequest
-import androidx.credentials.GetCredentialResponse
-import androidx.credentials.PasswordCredential
-import androidx.credentials.PublicKeyCredential
-import androidx.credentials.exceptions.GetCredentialException
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
-import com.google.android.libraries.identity.googleid.GetGoogleIdOption
-import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
-import com.google.android.libraries.identity.googleid.GoogleIdTokenParsingException
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.signin.internal.SignInHubActivity
+import com.google.android.gms.common.Scopes
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.Scope
+import com.google.android.gms.tasks.Task
+import com.part2.a3dify.R
 import com.part2.a3dify.app_containers.LoginFragmentContainer
 import com.part2.a3dify.app_containers.ThrDifyApplication
 import com.part2.a3dify.app_entry.viewmodels.LoginFragmentViewModel
@@ -40,88 +41,48 @@ class LoginFragment : Fragment() {
             return@viewModels temp.loginFragmentViewModelFactory
     }
 
+    private lateinit var gso: GoogleSignInOptions
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
 
-
-
-
-    fun handleSignIn(result: GetCredentialResponse) {
-        // Handle the successfully returned credential.
-        val credential = result.credential
-
-        when (credential) {
-//            is PublicKeyCredential -> {
-//                // Share responseJson such as a GetCredentialResponse on your server to
-//                // validate and authenticate
-//                responseJson = credential.authenticationResponseJson
-//            }
-
-            is PasswordCredential -> {
-                // Send ID and password to your server to validate and authenticate.
-                val username = credential.id
-                val password = credential.password
-                Log.d("TAG", username)
-                Log.d("TAG", password)
-            }
-
-            is CustomCredential -> {
-                if (credential.type == GoogleIdTokenCredential.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL) {
-                    try {
-                        // Use googleIdTokenCredential and extract id to validate and
-                        // authenticate on your server.
-                        val googleIdTokenCredential = GoogleIdTokenCredential
-                            .createFrom(credential.data)
-                    } catch (e: GoogleIdTokenParsingException) {
-                        Log.e("TAG", "Received an invalid google id token response", e)
-                    }
-                } else {
-                    // Catch any unrecognized custom credential type here.
-                    Log.e("TAG", "Unexpected type of credential")
-                }
-            }
-//
-            else -> {
-                // Catch any unrecognized credential type here.
-                Log.e("TAG", "Unexpected type of credential")
-            }
+    private val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {result->
+        Log.d("Launcher", "${result.resultCode}")
+        if (result.resultCode == Activity.RESULT_OK) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            handleSignInResult(task)
+            Log.d("Launcher", "Result OK")
         }
     }
 
+    private fun handleSignInResult(task: Task<GoogleSignInAccount>) {
+        try {
+            val account = task.getResult(ApiException::class.java)
+            // Signed in successfully, show authenticated UI or proceed with the account details.
+            Log.d("Sign In", "Successful")
 
-//    private lateinit var gso: GoogleSignInOptions
-//    private lateinit var mGoogleSignInClient: GoogleSignInClient
+            if (account != null) {
+                Log.d("email", "${account.email}")
+                Log.d("email", "${account.serverAuthCode}")
+                Log.d("email", "${account.idToken}")
+                Log.d("email", "${account.grantedScopes}")
+                Log.d("email", "${account.isExpired}")
+                Log.d("email", "${account.id}")
+            }
+        } catch (e: ApiException) {
+            // The ApiException status code indicates the detailed failure reason.
+            Log.w("API Error", "signInResult:failed code=" + e.statusCode)
+        }
+    }
 
-//    private val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {result->
-//        Log.d("Launcher", "Invoked")
-//        if (result.resultCode == Activity.RESULT_OK) {
-//            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
-//            handleSignInResult(task)
-//            Log.d("Launcher", "Result OK")
-//        }
-//    }
-
-//    private fun handleSignInResult(task: Task<GoogleSignInAccount>) {
-//        try {
-//            val account = task.getResult(ApiException::class.java)
-//            // Signed in successfully, show authenticated UI or proceed with the account details.
-//            Log.d("Sign In", "Successful")
-//        } catch (e: ApiException) {
-//            // The ApiException status code indicates the detailed failure reason.
-//            Log.w("API Error", "signInResult:failed code=" + e.statusCode)
-//        }
-//    }
+    private fun moveSignUpActivity() {
+        requireActivity().run {
+            startActivity(Intent(requireContext(), SignInHubActivity::class.java))
+            finish()
+        }
+    }
 
     // before view, check for login state of Google Account
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // Configure sign-in to request the user's ID, email address and basic profile.
-        // ID and basic profile are included in DEFAULT_SIGN_IN
-//        gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-//            .requestIdToken(SERVER_CLIENT_ID)
-//            .requestEmail()
-//            .build() // for addional scopes, specify them with 'requestScopes'
-//
-//        mGoogleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
     }
 
 
@@ -130,46 +91,38 @@ class LoginFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        // Configure sign-in to request the user's ID, email address and basic profile.
+        // ID and basic profile are included in DEFAULT_SIGN_IN
+        gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+//            .requestServerAuthCode(getString(R.string.server_client_id)) // Only for backend Server
+            .requestScopes(Scope(Scopes.DRIVE_APPFOLDER))
+            .requestEmail()
+            .build() // for addional scopes, specify them with 'requestScopes'
+
+        mGoogleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
+
         binding = FragmentLoginBinding.inflate(inflater)
 
-        val credentialManager = CredentialManager.create(requireContext())
-
-        val googleIdOption: GetGoogleIdOption = GetGoogleIdOption.Builder()
-            .setFilterByAuthorizedAccounts(true)
-            .setServerClientId(SERVER_CLIENT_ID)
-            .build()
-
-        val request: GetCredentialRequest = GetCredentialRequest.Builder()
-            .addCredentialOption(googleIdOption)
-            .build()
-
         binding.googleLoginButton.setOnClickListener {
-            lifecycleScope.launch {
-                try {
-                    val result = credentialManager.getCredential(
-                        request = request,
-                        context = requireContext()
-                    )
-                    handleSignIn(result)
-                } catch (e: GetCredentialException) {
-                    Log.d("TAG1", e.message.toString())
-                }
-            }
-        }
+            val account: GoogleSignInAccount? = GoogleSignIn.getLastSignedInAccount(requireContext())
 
-//        binding.googleLoginButton.setOnClickListener {
-//            val account: GoogleSignInAccount? = GoogleSignIn.getLastSignedInAccount(requireContext())
-//            if (account == null || account.idToken == null) {
+            mGoogleSignInClient.signOut()
+
+            val signInIntent = mGoogleSignInClient.signInIntent
+//            startActivityForResult(signInIntent, RC_SIGN_IN)
+            resultLauncher.launch(signInIntent)
+
+//            if (account == null) {
 //                val signInIntent = mGoogleSignInClient.signInIntent
 ////            startActivityForResult(signInIntent, RC_SIGN_IN)
 //                resultLauncher.launch(signInIntent)
 //            } else {
 //                Log.d("Account Check", "Existing")
 //                Log.d("email", "${account.email}")
-//                Log.d("email", "${account.idToken}")
+//                Log.d("email", "${account.serverAuthCode}")
 //                Log.d("email", "${account.id}")
 //            }
-//        }
+        }
 
         return binding.root
     }
